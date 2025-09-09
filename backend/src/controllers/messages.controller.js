0ae141fbaad4c829b4e3b1aba8598c0f2e9257c4
Ravi7035 <Ravimuliki7035@gmail.com>
2025-09-09 22:38:32 +0530

    added get messages route

--- backend/src/controllers/messages.controller.js
+++ backend/src/controllers/messages.controller.js
@@ -1,11 +1,12 @@
 import User from "../models/modeldb.js";
+import message from "../models/messagesdb.js";
 
-export const getusers=(req,res)=>
+export const getusers=async (req,res)=>
 {
     try{
     const userid=req.user._id;
 
-    const users=User.find({_id : {ne : userid}});
+    const users= await User.find({_id : {ne : userid}});
 
     res.status(201).json({
         users
@@ -19,3 +20,31 @@ export const getusers=(req,res)=>
         }
 }
 
+export const getmessages =async (req,res)=>
+{
+    try{
+
+        const {id:UsertoChat} = req.params;
+        const myId=req.user._id;
+
+        const messages=message.find(
+            {
+                $or:[
+                    {sender_id:myId,receiver_id:UsertoChat},
+                    {receiver_id:UsertoChat,sender_id:myId}
+                ]
+            }
+        );
+
+        res.status(201).json(
+            {
+                messages
+            }
+        );
+    }
+    catch(err)
+    {
+        res.status(400).send("internal server error");
+        console.log("error occurred",err.message);
+    }
+}
--- backend/src/routes/messages.routes.js
+++ backend/src/routes/messages.routes.js
@@ -1,11 +1,14 @@
 import express from "express";
 
 const router=express.Router();
-import {getusers} from "../controllers/messages.controller.js";
+import {getusers,getmessages} from "../controllers/messages.controller.js";
+
+
 
 import {protectroute} from "../middleware/auth.middleware.js"
 
 router.get("/users",protectroute,getusers);
+router.get("/:id",protectroute,getmessages);
 
 
 export default router;
