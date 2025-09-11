1b78505e7ec81b8cb9f9b7b1ac35a49e797c8d19
Ravi7035 <Ravimuliki7035@gmail.com>
2025-09-11 11:33:15 +0530

    ----

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -1,11 +1,11 @@
 
 import {Routes,Route} from "react-router-dom";
-import Navbar from "./components/Navbar.jsx";
+import Navbar from "./components/Navbar.jsx"
 import Homepage from "./pages/Homepage.jsx";
-import Homepage from "./pages/Homepage.jsx";
-import Homepage from "./pages/Homepage.jsx";
-import Homepage from "./pages/Homepage.jsx";
-import Homepage from "./pages/Homepage.jsx";
+import Loginpage from "./pages/Loginpage.jsx";
+import Signuppage from "./pages/Signuppage.jsx";
+import Settingspage from "./pages/Settingspage.jsx";
+import Profilepage from "./pages/Profilepage.jsx";
 
 function App() {
  
@@ -15,10 +15,10 @@ function App() {
       <Navbar/>
       <Routes>
         <Route path="/" element={<Homepage/>}/>
-        <Route path="/signup" element={<Signup/>}/>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/settings" element={<Settings/>}/>
-        <Route path="/profile" element={<Profile/>}/>
+        <Route path="/signup" element={<Signuppage/>}/>
+        <Route path="/login" element={<Loginpage/>}/>
+        <Route path="/settings" element={<Settingspage/>}/>
+        <Route path="/profile" element={<Profilepage/>}/>
       </Routes>
 
     </div>
