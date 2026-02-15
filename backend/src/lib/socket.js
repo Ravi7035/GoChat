56e24d85fd392fa7d4a25551fe67a8725fd9c5ff
Ravi7035 <Ravimuliki7035@gmail.com>
2026-02-15 20:07:26 +0530

    ..

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -1,21 +1,28 @@
-import {Server} from "socket.io";
+import { Server } from "socket.io";
 import http from "http";
 import express from "express";
 
+const app = express();
+const server = http.createServer(app);
 
-const app =express();
-const server= http.createServer(app);
-const io=new Server(server,{
-    cors:{
-        origin:["http://localhost:5173"],
-    },
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://gochat-1-vpu4.onrender.com"
+];
+
+const io = new Server(server, {
+  cors: {
+    origin: allowedOrigins,
+    credentials: true
+  },
 });
-io.on("connection",(socket)=>
-{ 
-    console.log("a user  connected",socket.id);
-    socket.on("disconnect",()=>
-    {
-        console.log(" a user disconnected",socket.id);
-    })
-})
-export {io,app,server};
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
+export { io, app, server };
