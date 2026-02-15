1b7d414e9a6e1579eea0ed9357161389099d0733
Ravi7035 <Ravimuliki7035@gmail.com>
2026-02-15 19:50:37 +0530

    render error fixed

--- frontend/vite-project/src/lib/axios.js
+++ frontend/vite-project/src/lib/axios.js
@@ -3,6 +3,6 @@ import axios from "axios";
  export const axiosInstance = axios.create({
   baseURL: import.meta.env.PROD 
   ? "" 
-  : "http://localhost:5002",
+  : "http://localhost:5002/api",
   withCredentials: true
 });
